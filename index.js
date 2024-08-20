2e31e08c18d635215f6dadd6022ab1900a72b2fc
Kushal Agrawal <agrawalskushal@gmail.com>
2024-08-20 20:46:41 +0530

    Mongo remote connection

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -13,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect('mongodb://root:admin@localhost:27017/reposight?authSource=admin', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
